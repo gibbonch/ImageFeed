6836c5e9e36cf69f8f0aadb9cdf0abaabc7cdb95
gibbonch <toropov280304@icloud.com>
2024-05-10 16:44:43 +0300

    Add dataSource, delegate for tableView

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -22,3 +22,25 @@ final class ImagesListViewController: UIViewController {
     
 }
 
+// MARK: - UITableViewDataSource
+
+extension ImagesListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
+// MARK: - UITableViewDelegate
+
+extension ImagesListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    
+}
+
