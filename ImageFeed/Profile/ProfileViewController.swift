77158b21fcd636ed0ed5d9d06d2545c6a2f876f6
gibbonch <toropov280304@icloud.com>
2024-07-18 20:24:00 +0300

    Add Profile screen Layout

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Александр Торопов on 18.07.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBAction private func logoutButtonDidTap(_ sender: Any) { }
+}
