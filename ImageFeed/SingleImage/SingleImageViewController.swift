825c5acb338cb297ef774c41df294a4279659860
gibbonch <toropov280304@icloud.com>
2024-07-21 16:42:06 +0300

    Add zooming feature on SingleImage screen

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -9,14 +9,26 @@ import UIKit
 
 final class SingleImageViewController: UIViewController {
     
-    var image: UIImage?
-    
     @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var scrollView: UIScrollView!
+    
+    @IBOutlet private weak var imageViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var imageViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var imageViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var imageViewTrailingConstraint: NSLayoutConstraint!
+    
+    var image: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureImageView()
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        updateZoomScale(for: view.bounds.size)
     }
     
     @IBAction private func backwardButtonDidTap(_ sender: Any) {
@@ -25,6 +37,40 @@ final class SingleImageViewController: UIViewController {
     
     private func configureImageView() {
         imageView.image = image
+    }
+    
+    private func updateZoomScale(for size: CGSize) {
+        let hScale = size.width / scrollView.contentSize.width
+        let vScale = size.height / scrollView.contentSize.height
+        let minScale = min(hScale, vScale)
+        
+        scrollView.minimumZoomScale = minScale
+        scrollView.zoomScale = minScale
+    }
+    
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let size = view.bounds.size
+        updateConstraints(for: size)
+    }
+    
+    func updateConstraints(for size: CGSize) {
+        let yOffset = max(0, (size.height - imageView.frame.height) / 2)
+        imageViewTopConstraint.constant = yOffset
+        imageViewBottomConstraint.constant = yOffset
+        
+        let xOffset = max(0, (size.width - imageView.frame.width) / 2)
+        imageViewLeadingConstraint.constant = xOffset
+        imageViewTrailingConstraint.constant = xOffset
+        
+        view.layoutIfNeeded()
     }
     
 }
